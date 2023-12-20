c3baddef0d4b71aa008e30a04aba47d11528bb02
Sagor Hossain <sagorhassain4@gmail.com>
2023-12-21 00:22:29 +0600

    PascalsTriangle

--- 118_PascalsTriangle.py
+++ 118_PascalsTriangle.py
@@ -1,1 +1,26 @@
-print('test')
+from typing import List
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        pascal_arr = [[1]]
+
+        for i in range(1, numRows):
+            previous = pascal_arr[i-1]
+            current = [1]
+
+            for j in range(1, i):
+                current.append(previous[j-1] + previous[j])
+
+            current.append(1)
+            pascal_arr.append(current)
+        return pascal_arr
+
+# Create an instance of the class
+solution_instance = Solution()
+
+# Call the generate method
+numRows = 2
+pascal_arr = solution_instance.generate(numRows)
+print(pascal_arr)
